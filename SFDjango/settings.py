dcfb6558131ab7c8d7dedb03b7ae6dde6722efb6
Vinícius de Sousa Santana <sousavinicius@hotmail.com>
2021-04-09 16:20:31 -0300

    Reconfigured to display in UTF-8.

--- SFDjango/settings.py
+++ SFDjango/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
